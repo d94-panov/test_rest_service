2de5b1f15b85568e86aa1c14a10adc172feac2c8
Dmitriy <d94-panov@mail.ru>
2023-01-20 18:47:18 +0500

    lab2

--- src/main/java/com/students/test_rest_service/controller/MyController.java
+++ src/main/java/com/students/test_rest_service/controller/MyController.java
@@ -11,9 +11,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.beans.factory.annotation.Qualifier;
-//import com.students.test_rest_service.service.MyModifyService;
 
 @Slf4j
 @RestController
